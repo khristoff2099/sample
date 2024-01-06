959373e0dc3bc31455fe3e671ca08a0816c57d39
Christopher <52580088+khristoff2099@users.noreply.github.com>
2024-01-06 14:57:37 -0600

    EmployeeProject
    EmployeeProject

--- rest-api-master/src/main/java/com/martscompany/restapi/interfaces/EmployeeWHRepository.java
+++ rest-api-master/src/main/java/com/martscompany/restapi/interfaces/EmployeeWHRepository.java
@@ -4,8 +4,8 @@ package com.martscompany.restapi.interfaces;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
@@ -20,14 +20,14 @@ public interface EmployeeWHRepository extends JpaRepository<EmployeeWH, Integer>
 	    @Modifying
 	 @Query(value = " SELECT w.worked_hours FROM employee_worked_hours w inner join employee e on e.id = w.employee_id "
 	 		//+ "WHERE w.worked_hours < 20 and e.id=? ", nativeQuery = true)
-	 + "WHERE w.worked_hours < 20 and e.id=? ")
+	 + "WHERE w.worked_hours < 20 and e.id=? ", nativeQuery = true)
      public Optional<EmployeeWH> findByWH(@Param("id") Integer id);
 	 
 	 @Transactional
 	    @Modifying
 	 @Query(value = "SELECT w.worked_hours FROM employee_worked_hours w INNER JOIN  employee e ON w.employee_id = e.id "
 	 		//+ "WHERE w.worked_date between ? and ? ", nativeQuery = true)
-	 + "WHERE w.worked_date between ? and ? ")
+	 + "WHERE w.worked_date between ? and ? ", nativeQuery = true)
 	 public ResponseEntity<ExceptionMessages> findByWD(@Param("worked_date") String val,@Param("worked_date") String val2);
 
 }
--- rest-api-master/src/main/java/com/martscompany/restapi/interfaces/JobsRepository.java
+++ rest-api-master/src/main/java/com/martscompany/restapi/interfaces/JobsRepository.java
@@ -3,9 +3,9 @@ package com.martscompany.restapi.interfaces;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
@@ -21,6 +21,6 @@ public interface JobsRepository extends JpaRepository<Jobs, Integer>, JpaSpecifi
 	    @Modifying
 	 @Query(value = "SELECT j.salary from jobs j inner join employee e on e.job_id = j.id inner join employee_worked_hours w on w.employee_id = e.id "
 	 		//+ "WHERE w.worked_date between ? and ? ", nativeQuery = true)
-			 + "WHERE w.worked_date between ? and ? ")
+			 + "WHERE w.worked_date between ? and ? ", nativeQuery = true)
 	 public ResponseEntity<ExceptionMessages> findByWS(@Param("worked_date") String val,@Param("worked_date") String val2);
 }
