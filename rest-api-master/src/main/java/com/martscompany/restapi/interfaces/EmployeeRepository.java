6e9248470628b385f356123613b6eee6fa11b262
Christopher <52580088+khristoff2099@users.noreply.github.com>
2024-01-06 21:19:33 -0600

    EmplyeeProject
    EmplyeeProject

--- rest-api-master/src/main/java/com/martscompany/restapi/interfaces/EmployeeRepository.java
+++ rest-api-master/src/main/java/com/martscompany/restapi/interfaces/EmployeeRepository.java
@@ -3,13 +3,14 @@ package com.martscompany.restapi.interfaces;
 import javax.transaction.Transactional;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import com.martscompany.restapi.entity.Employee;
 import com.martscompany.restapi.exceptions.ExceptionMessages;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {	
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>, JpaSpecificationExecutor<Employee> {	
 	 @Transactional
 	    @Modifying
 	 @Query(value = "SELECT * FROM employee e"
--- rest-api-master/src/main/java/com/martscompany/restapi/interfaces/GendersRepository.java
+++ rest-api-master/src/main/java/com/martscompany/restapi/interfaces/GendersRepository.java
@@ -1,6 +1,5 @@
 package com.martscompany.restapi.interfaces;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.martscompany.restapi.entity.Genders;
 
