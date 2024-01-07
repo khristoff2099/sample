2d123b5aacc6b3b2b639e394178237909da2ac70
Christopher <52580088+khristoff2099@users.noreply.github.com>
2024-01-06 18:10:50 -0600

    SampleProject
    SampleProject

--- rest-api-master/src/main/java/com/martscompany/restapi/interfaces/JobsRepository.java
+++ rest-api-master/src/main/java/com/martscompany/restapi/interfaces/JobsRepository.java
@@ -1,7 +1,6 @@
 package com.martscompany.restapi.interfaces;
 
 import javax.transaction.Transactional;
-
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -9,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-
 import com.martscompany.restapi.entity.EmployeeWH;
 import com.martscompany.restapi.entity.Jobs;
 import com.martscompany.restapi.exceptions.ExceptionMessages;
