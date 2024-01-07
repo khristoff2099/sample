0c16059774b3a28f15c1ff36bec3316fb9170d50
Christopher <52580088+khristoff2099@users.noreply.github.com>
2024-01-06 21:17:17 -0600

    EmployeeProject
    EmployeeProject

--- rest-api-master/src/main/java/com/martscompany/restapi/interfaces/EmployeeRepository.java
+++ rest-api-master/src/main/java/com/martscompany/restapi/interfaces/EmployeeRepository.java
@@ -1,13 +1,11 @@
 package com.martscompany.restapi.interfaces;
 
 import javax.transaction.Transactional;
-
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-
 import com.martscompany.restapi.entity.Employee;
 import com.martscompany.restapi.exceptions.ExceptionMessages;
 
--- rest-api-master/src/main/java/com/martscompany/restapi/interfaces/JobsRepository.java
+++ rest-api-master/src/main/java/com/martscompany/restapi/interfaces/JobsRepository.java
@@ -1,12 +1,8 @@
 package com.martscompany.restapi.interfaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-import com.martscompany.restapi.entity.EmployeeWH;
 import com.martscompany.restapi.entity.Jobs;
 
-@Repository
-public interface JobsRepository extends JpaRepository<Jobs, Integer>, JpaSpecificationExecutor<EmployeeWH> {
+public interface JobsRepository extends JpaRepository<Jobs, Integer> {
 
 }
